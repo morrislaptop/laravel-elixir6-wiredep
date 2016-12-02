8de38400986d28d5af67458f380780bdba6f1690
Craig Morris <craigmorris@Craigs-MBP-Touch.local>
2016-12-02 00:04:55 +0000

    Fix bug

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ Elixir.extend(
   'wiredep',
   function(type, src, baseDir, options = {}) {
     const config = require('./config')
+
     src = src || config[type].config.src
     baseDir = baseDir || config[type].config.baseDir
+    options = Object.assign({}, config[type].opts, options)
+
     new WiredepTask(
       'wiredep', getPaths(src, baseDir), options
     );
